a343274d25039069498cb86e2697028b2df40fd7
Jonathon Schwartz <Jonathon.schwartz12@gmail.com>
2019-06-03 18:07:04 +1000

    formatted setup file a tad

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 setup(name="rlalgs",
       version="0.0.1",
       install_requires=[
-        'gym',
-        'scipy',
-        'numpy',
-        'mpi4py',
-        'pandas',
-        'pyyaml',
-        'matplotlib',
-        'prettytable'
+          'gym',
+          'scipy',
+          'numpy',
+          'mpi4py',
+          'pandas',
+          'pyyaml',
+          'matplotlib',
+          'prettytable'
       ],
       description="Implementations of some RL algorithms, based off of OpenAI spinningup tutorials",
       author="Jonathon Schwartz",
