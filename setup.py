47cd5a9d2422d5c1c35fdc8a2a956383cdb27221
Jonathon Schwartz <Jonathon.schwartz12@gmail.com>
2019-06-14 16:15:44 +1000

    Setup package install for tensorflow 2.0

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
+import sys
 from setuptools import setup
+
+assert sys.version_info.major == 3 and sys.version_info.minor >= 7, \
+    "The rlalgs repo is designed to work with Python 3.7 and greater." \
+    + "Please install it before proceeding."
 
 setup(name="rlalgs",
       version="0.0.1",
@@ -10,7 +15,8 @@ setup(name="rlalgs",
           'pandas',
           'pyyaml',
           'matplotlib',
-          'prettytable'
+          'prettytable',
+          'tensorflow==2.0.0-beta0'
       ],
       description="Implementations of some RL algorithms, based off of OpenAI spinningup tutorials",
       author="Jonathon Schwartz",
