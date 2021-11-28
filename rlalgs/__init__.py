00aea6d097a715b2fef209150f118f83e632a5f5
Jonathon <Jonathon.schwartz12@gmail.com>
2021-11-28 10:03:54 -0500

    Updated setup

--- rlalgs/__init__.py
+++ rlalgs/__init__.py
@@ -1,9 +1,17 @@
+# Just disables the warning, doesn't enable AVX/FMA
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 # Algorithms
 from rlalgs.algos.a2c.a2c import a2c    # noqa
 from rlalgs.algos.dqn.dqn import dqn    # noqa
 from rlalgs.algos.simplepg.simplepg import simplepg     # noqa
 from rlalgs.algos.vpg.vpg import vpg    # noqa
 
-# Just disables the warning, doesn't enable AVX/FMA
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+try:
+    import tensorflow
+except ModuleNotFoundError:
+    print(
+        "Tensorflow not installed see 'https://www.tensorflow.org/install' "
+        "for instructions"
+    )
--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
 import sys
 from setuptools import setup
 
-assert sys.version_info.major == 3 and sys.version_info.minor >= 7, \
+assert sys.version_info.major == 3 and sys.version_info.minor == 9, \
     "The rlalgs repo is designed to work with Python 3.7 and greater." \
     + "Please install it before proceeding."
 
-setup(name="rlalgs",
-      version="0.0.1",
-      install_requires=[
-          'gym',
-          'scipy',
-          'numpy',
-          'mpi4py',
-          'pandas',
-          'pyyaml',
-          'psutil',
-          'matplotlib',
-          'prettytable',
-          'tensorflow==2.0.0-beta0'
-      ],
-      description="Implementations of some RL algorithms, based off of OpenAI spinningup tutorials",
-      author="Jonathon Schwartz",
-      )
+setup(
+    name="rlalgs",
+    version="0.0.1",
+    install_requires=[
+        'gym',
+        'scipy',
+        'numpy',
+        # 'mpi4py',
+        'pandas',
+        'pyyaml',
+        'psutil',
+        'matplotlib',
+        'prettytable',
+    ],
+    description=(
+        "Implementations of some RL algorithms, based off of OpenAI "
+        "spinningup tutorials"
+    ),
+    author="Jonathon Schwartz",
+)
